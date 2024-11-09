ff084edfcde3b92768a7c6fefb8ce61bf7395534
Guntis <gb322400@gmail.com>
2024-11-09 12:29:09 -0500

    Complited task 1-6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,9 +12,9 @@ public_users.post("/register", (req,res) => {
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
-        if (!doesExist(username)) {
-            // Add the new user to the users array
-            users.push({"username": user2, "password": password2});
+        if (!isValid(username)) {
+            // Add the new user to the users array6-register
+            users.push({"username": username, "password": password});
             return res.status(200).json({message: "User successfully registered. Now you can login"});
         } else {
             return res.status(404).json({message: "User already exists!"});
@@ -25,43 +25,102 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-// public_users.get('/',function (req, res) {
+ public_users.get('/',function (req, res) {
   //Write your code here
-  // res.send(JSON.stringify(books,null,4));
-  // return res.status(300).json({message: "Yet to be implemented"});
-// });
+   res.send(JSON.stringify(books,null,4));
+   return res.status(300).json({message: "Yet to be implemented"});
+ });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     try {
-        const requestedIsbn = req.params.isbn; // Retrieve ISBN from request parameters
+        const requestedIsbn = req.params.isbn;
         const book = books[requestedIsbn];
         if (book) {
-          res.json(book); // Send the book details as a JSON response
+          res.json(book);
         } else {
-          res.status(404).json({ message: "Book not found" }); // Handle book not found
+          res.status(404).json({ message: "Book not found" });
         }
       } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Error retrieving book details" }); // Handle unexpected errors
+        res.status(500).json({ message: "Error retrieving book details" });
       }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    try {
+        const requestedAuthor = req.params.author;
+        const matchingBooks = [];
+    
+        
+        const bookKeys = Object.keys(books);
+    
+        
+        for (const key of bookKeys) {
+          const book = books[key];
+          if (book.author === requestedAuthor) {
+            matchingBooks.push(book);
+          }
+        }
+    
+        if (matchingBooks.length > 0) {
+          res.json(matchingBooks);
+        } else {
+          res.status(404).json({ message: "No books found by that author" });
+        }
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error retrieving books" });
+      }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try {
+    const requestedTitle = req.params.title;
+    const matchingBooks = [];
+
+    
+    const bookKeys = Object.keys(books);
+
+    
+    for (const key of bookKeys) {
+      const book = books[key];
+      if (book.title.toLowerCase() === requestedTitle.toLowerCase()) { 
+        matchingBooks.push(book);
+      }
+    }
+
+    if (matchingBooks.length > 0) {
+      res.json(matchingBooks);
+    } else {
+      res.status(404).json({ message: "No books found with that title" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving books" });
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
+  try {
+    const requestedIsbn = req.params.isbn;
+    const book = books[requestedIsbn];
+
+    if (book) {
+      const reviews = book.reviews;
+      res.json(reviews);
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving reviews" });
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
